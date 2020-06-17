01f20d346d0f500ff35515d2c4c7859eaf54bfb1
raynardbrown <35472906+raynardbrown@users.noreply.github.com>
2020-06-16 23:43:44 -0500

    feat: Add fsman_resource.h

---
+++ res/fsman_resource.h
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+// File: fsman_resource.h
+//
+// Author: Raynard Brown
+//
+// Copyright (c) 2020 Raynard Brown
+//
+// All rights reserved.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef FSMAN_RESOURCE_H_
+#define FSMAN_RESOURCE_H_
+
+#define COMPANY_NAME_STRING       "Raynard Brown\0"
+#define LEGAL_COPYRIGHT_STRING    "Copyright \251 2020 Raynard Brown\0"
+#define LEGAL_TRADEMARKS_STRING   "Copyright \251 2020 Raynard Brown\0"
+
+#define COMMENTS_STRING           "Additional information available at https://github.com/raynardbrown/fsman\0"
+
+#define FILE_VERSION              0,0,1,0
+#define FILE_VERSION_STRING       "0.0.1.0\0"
+
+#define PRODUCT_VERSION           0,0,1,0
+#define PRODUCT_VERSION_STRING    "0.0.1.0\0"
+
+#define INTERNAL_NAME_STRING      "fsman\0"
+#define FILE_DESCRIPTION_STRING   "FS Man\0"
+#define ORIGINAL_FILENAME_STRING  "fsman.exe\0"
+#define PRODUCT_NAME_STRING       "FS Man\0"
+
+#endif /* FSMAN_RESOURCE_H_ */
